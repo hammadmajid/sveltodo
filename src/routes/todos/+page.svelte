5285aed4ce7247638e0d71441e7bb01a7564a5e8
Hammad Majid <hammadmajid@proton.me>
2024-09-10 23:25:33 +0500

    create very basic /todos pages

---
+++ src/routes/todos/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { onMount } from 'svelte';
+
+	let todos: string | undefined | null = $state();
+
+	onMount(() => {
+		todos = localStorage.getItem('todos.txt');
+	});
+</script>
+
+<main class="mx-auto my-12 space-y-4 px-4 md:w-1/2">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Todos</h1>
+	<div class="bg-muted px-4 py-8">
+		{#if todos}
+			{todos}
+		{:else}
+			<p>No todos left.</p>
+		{/if}
+	</div>
+</main>
+
+<footer>
+	<Button href="/settings" variant="link">Settings</Button>
+</footer>
