7a3292cab2780a0a85779d3c0ae37bb214eb02f4
Hammad Majid <hammadmajid@proton.me>
2024-09-11 01:33:19 +0500

    let user manually edit the todo.txt

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,25 +1,40 @@
 <script lang="ts">
-	import { ChevronLeft, Github, Moon, Sun } from 'lucide-svelte';
+	import { ChevronLeft, Github, Moon, SquarePen, Sun, Trash } from 'lucide-svelte';
 
 	import { goto } from '$app/navigation';
 	import * as AlertDialog from '$lib/components/ui/alert-dialog';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-	import type { CustomEventHandler } from 'bits-ui';
+	import type { ButtonEventHandler, CustomEventHandler } from 'bits-ui';
 	import { resetMode, setMode } from 'mode-watcher';
 	import { onMount } from 'svelte';
+	import { Textarea } from '$lib/components/ui/textarea';
+	import * as Dialog from '$lib/components/ui/dialog';
+
+	let todos: string;
 
 	let deleteTodoTxt: (e: CustomEventHandler<MouseEvent, HTMLButtonElement>) => void;
+	let saveTodoTxt: { (): void; (e: ButtonEventHandler<MouseEvent>): void };
 
 	onMount(() => {
 		deleteTodoTxt = () => {
 			localStorage.removeItem('todos.txt');
 			goto('/');
 		};
+
+		saveTodoTxt = () => {
+			localStorage.setItem('todos.txt', todos);
+		};
+
+		const getTodos = () => {
+			return localStorage.getItem('todos.txt') as string;
+		};
+
+		todos = getTodos();
 	});
 </script>
 
-<main class="mx-auto my-12 space-y-12 px-4 md:w-1/2">
+<main class="lg:1/2 mx-auto my-12 space-y-12 px-4 md:w-3/4">
 	<div class="space-y-4">
 		<Button class="p-0" href="/todos" variant="link"><ChevronLeft /> Go back</Button>
 		<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Settings</h1>
@@ -47,16 +62,48 @@
 			</DropdownMenu.Root>
 		</div>
 		<div class="flex items-center justify-between">
+			<p>Manual edit</p>
+
+			<Dialog.Root>
+				<Dialog.Trigger>
+					<Button variant="outline" class="flex items-center gap-2"
+						><SquarePen class="w-4" /> Edit</Button
+					>
+				</Dialog.Trigger>
+				<Dialog.Content>
+					<Dialog.Header>
+						<Dialog.Title>
+							<span class="flex items-center gap-2">
+								<SquarePen />
+								Edit
+							</span>
+						</Dialog.Title>
+						<Dialog.Description>Manually edit your todo.txt file.</Dialog.Description>
+					</Dialog.Header>
+					<div class="grid w-full gap-2">
+						<Textarea bind:value={todos} placeholder="Nothing to see here." />
+					</div>
+					<Dialog.Footer>
+						<Button type="submit" on:click={saveTodoTxt}>Save changes</Button>
+					</Dialog.Footer>
+				</Dialog.Content>
+			</Dialog.Root>
+		</div>
+		<div class="flex items-center justify-between">
 			<p>Delete todo.txt</p>
 			<AlertDialog.Root>
 				<AlertDialog.Trigger>
-					<Button variant="destructive">Delete</Button>
+					<Button variant="destructive" class="flex items-center gap-2">
+						<Trash class="w-4" />
+						Delete
+					</Button>
 				</AlertDialog.Trigger>
 				<AlertDialog.Content>
 					<AlertDialog.Header>
 						<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
 						<AlertDialog.Description>
-							This action cannot be undone. This will permanently delete your todo.txt file stored in localStorage.
+							This action cannot be undone. This will permanently delete your todo.txt file stored
+							in localStorage.
 						</AlertDialog.Description>
 					</AlertDialog.Header>
 					<AlertDialog.Footer>
